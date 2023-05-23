9dc7495a88a01248d11c0bdcd42424945f613179
Anchal Gupta <guptaanchal373@gmail.com>
2023-05-23 08:14:02 +0530

    Changes in String_Practice

--- src/BasicConcept/StringPractice.java
+++ src/BasicConcept/StringPractice.java
@@ -1,5 +1,11 @@
 package BasicConcept;
 
 public class StringPractice {
+	
+	public static void main(String[] args) {
+		System.out.println("Stash on branch A");
+		System.out.println("I am in method of Branch A");
+		System.out.println("Conflict create");
+	}
 
 }
