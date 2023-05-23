32fde92c71e75cf8a378472b4ec569fed261a070
Anchal Gupta <guptaanchal373@gmail.com>
2023-05-23 08:43:02 +0530

    conflict resolve

--- src/BasicConcept/StringPractice.java
+++ src/BasicConcept/StringPractice.java
@@ -3,9 +3,9 @@ package BasicConcept;
 public class StringPractice {
 	
 	public static void main(String[] args) {
-		System.out.println("Stash on branch A");
 		System.out.println("I am in method of Branch A");
 		System.out.println("Conflict create");
+		System.out.println("In branch B1 creating conflict");
 	}
 
 }
